2058708d761089c3f15b2f93fcd30ce9233c00e2
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-01-16 16:39:11 -0300

    createSprite

--- gallows/js/sprite.js
+++ gallows/js/sprite.js
@@ -1,13 +1,38 @@
-var sprite = $('.sprite');
-
-function nextFrame() {
-  let frame = 'frame';
+var createSprite = function(selector) {
+  var $el = $(selector);
+  var current = 0;
   
-    
-    sprite.addClass('frame3')
-    console.log(frame);
+  var frames = [
+    'frame1', 'frame2', 'frame3', 'frame4', 'frame5',
+    'frame6', 'frame7', 'frame8', 'frame9'
+  ];
+
+  var last = frames.length - 1;
+
+  var moveFrame = function(from, to) {
+    $el.removeClass(from)
+      .addClass(to);
+  }
+  var hasNext = function() {
+    return current + 1 <= last;
+  }
+
+  var nextFrame = function() {
+    if(hasNext()) {
+      moveFrame(frames[current], frames[++current]);
+    }
+  }
+  var reset = function () {
+    moveFrame(frames[current], frames[0]);
+    current = 0;
+  }
+  var isFinished = function() {
+    return !hasNext();
+  }
+ return {
+  nextFrame: nextFrame,
+  reset: reset,
+  isFinished: isFinished
+ }
 }
-
-nextFrame()
-
 
